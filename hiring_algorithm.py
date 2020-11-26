28dd672e7995a14b503c78f1df9120d5245fde45
James Hyojae Lee <49330578+jameshyojaelee@users.noreply.github.com>
2020-11-26 15:17:47 -0800

    Update hiring_algorithm.py

--- hiring_algorithm.py
+++ hiring_algorithm.py
@@ -5,8 +5,3 @@ filepath = 'C:/Users/james/Desktop/POLI170 Policy memo/RA_hiring_algorithm/RA_da
 
 RA_data = pd.read_csv(filepath)
 print(RA_data.iloc[:5])
-
-#con = 
-#path = 'C:/Users/james/Desktop/POLI170 Policy memo/RA_hiring_algorithm/RA_dataset.sql'
-#query = ''' SELECT * FROM RA_dataset; '''
-#data = pd.read_sql(query, con)
